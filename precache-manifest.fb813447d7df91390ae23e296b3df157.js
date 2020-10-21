8a6ed8f1c5a5cfd56bf6e84758d5e730d54aab13
vikas <vikas.gupta@ambab.com>
2020-10-21 15:28:07 +0530

    Updates

---
+++ precache-manifest.fb813447d7df91390ae23e296b3df157.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa341df0edbabc614fd90b11e62cb64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df5b051aaa70d61c3d9f",
+    "url": "/static/css/1.828be9a0.chunk.css"
+  },
+  {
+    "revision": "82838bf97fea64c53820",
+    "url": "/static/css/2.bbe4ddbe.chunk.css"
+  },
+  {
+    "revision": "a03832190340ecca5a21",
+    "url": "/static/css/main.5f5a7810.chunk.css"
+  },
+  {
+    "revision": "665e197be7ec4fb600d5",
+    "url": "/static/js/0.529b32a0.chunk.js"
+  },
+  {
+    "revision": "df5b051aaa70d61c3d9f",
+    "url": "/static/js/1.f831a6db.chunk.js"
+  },
+  {
+    "revision": "1790214efba34982a154466015e58e1d",
+    "url": "/static/js/1.f831a6db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "410c640783696597aea3",
+    "url": "/static/js/10.77e95b25.chunk.js"
+  },
+  {
+    "revision": "6c61145db4b2b6c204a6",
+    "url": "/static/js/11.50263d5e.chunk.js"
+  },
+  {
+    "revision": "9bf4aa319f79589d3b23",
+    "url": "/static/js/12.96086cd2.chunk.js"
+  },
+  {
+    "revision": "c85d62b7d2fda22041f2",
+    "url": "/static/js/13.69c659c5.chunk.js"
+  },
+  {
+    "revision": "7ae0c52b5c61ed4ccba6",
+    "url": "/static/js/14.e669a94a.chunk.js"
+  },
+  {
+    "revision": "82838bf97fea64c53820",
+    "url": "/static/js/2.1791a945.chunk.js"
+  },
+  {
+    "revision": "e7ea479d201397994db797ad41e5885f",
+    "url": "/static/js/2.1791a945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ffcf1d2ac43a76f07bd",
+    "url": "/static/js/5.3d9848bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/5.3d9848bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5a72b7a012e34a8edb",
+    "url": "/static/js/6.ce7c4efc.chunk.js"
+  },
+  {
+    "revision": "852114534c69db109ecc",
+    "url": "/static/js/7.4279bffb.chunk.js"
+  },
+  {
+    "revision": "a1bda8bc3d1e34186b19",
+    "url": "/static/js/8.d650875e.chunk.js"
+  },
+  {
+    "revision": "6f4ec041c04592365785",
+    "url": "/static/js/9.ab613518.chunk.js"
+  },
+  {
+    "revision": "a03832190340ecca5a21",
+    "url": "/static/js/main.13c5ec7c.chunk.js"
+  },
+  {
+    "revision": "bd731bd54c70477d17e7",
+    "url": "/static/js/runtime-main.e7965c83.js"
+  },
+  {
+    "revision": "763b9baad3de1b202240e1a2130509a1",
+    "url": "/static/media/AskmeInfoline.763b9baa.jpg"
+  },
+  {
+    "revision": "11a97fad9a3989da9205e896c5220a82",
+    "url": "/static/media/Harshit.11a97fad.jpg"
+  },
+  {
+    "revision": "907edd8dc56255e18df23ad6eb131b7f",
+    "url": "/static/media/Mayur.907edd8d.png"
+  },
+  {
+    "revision": "9d1a7a88317a218062370794fdcb71b5",
+    "url": "/static/media/Mitul.9d1a7a88.jpg"
+  },
+  {
+    "revision": "26593c10ce4b9b6d0e867f84fc99c0d6",
+    "url": "/static/media/Neha.26593c10.png"
+  },
+  {
+    "revision": "2a700d7ae04fe5e1a18aa5bef5107e34",
+    "url": "/static/media/Pooja.2a700d7a.jpg"
+  },
+  {
+    "revision": "bc77475d6a684d9459a2c5419518d009",
+    "url": "/static/media/Pratik.bc77475d.png"
+  },
+  {
+    "revision": "d3c33e84ca90ebbdb3e5ed3c1993f028",
+    "url": "/static/media/Sameer.d3c33e84.png"
+  },
+  {
+    "revision": "ccb5b1fa85f8018c424869576d414afb",
+    "url": "/static/media/Sanjeev.ccb5b1fa.jpg"
+  },
+  {
+    "revision": "3829d2e50c2400231e6bacb953ffa8b4",
+    "url": "/static/media/Vikas.3829d2e5.png"
+  },
+  {
+    "revision": "597175a03618e6de417aea54a13d921b",
+    "url": "/static/media/about-2.597175a0.jpg"
+  },
+  {
+    "revision": "a19f1317909eddc20bf8b1481c14e0a2",
+    "url": "/static/media/askmeinfoline.a19f1317.jpeg"
+  },
+  {
+    "revision": "b1870d3af26f3745d81e6140431ba293",
+    "url": "/static/media/bg_1.b1870d3a.jpg"
+  },
+  {
+    "revision": "e4b89b9ef0d4b6a7fdd903487528f9b9",
+    "url": "/static/media/bg_2.e4b89b9e.jpg"
+  },
+  {
+    "revision": "076c31eef7de88f8afd9ba73d24216d3",
+    "url": "/static/media/forextwist.076c31ee.jpeg"
+  },
+  {
+    "revision": "9f547c69ab6717ea702c3791b6c436fb",
+    "url": "/static/media/piyalzinal.9f547c69.jpeg"
+  },
+  {
+    "revision": "a6c56ec0ed3df9b74b2b01b2fcfadeb8",
+    "url": "/static/media/zionkart.a6c56ec0.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/freelancerdevs/precache-manifest.8f56c049cf13806dd95b576265c81be7.js"
+  "/precache-manifest.fb813447d7df91390ae23e296b3df157.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/freelancerdevs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
